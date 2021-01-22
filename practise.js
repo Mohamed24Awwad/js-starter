ef578d49192086a7c178d4f782c0eddbec180496
Muhammed <mohamedawwad578@gmail.com>
2021-01-22 18:49:43 +0200

    practise file Created

---
+++ practise.js
@@ -0,0 +1,15 @@
+//================= fibonacci Series =====================
+
+// 1,1,2,3,4,5,6
+//Every number is equavlint to the sum of the two previous number 
+
+var num1 = 0 ;
+var num2 = 1 ;
+var result = num2;
+
+for(var i = 1 ; i < 10 ; i++){
+   console.log(`${result}`);
+   result = num1 + num2 ;
+   num1 = num2 ;
+   num2 = result
+}
